c1976dbb693852141a158b627995a318ce35ce66
Frank Hollywood <github@srvsync.com>
2021-03-26 14:19:11 -0700

    started javascript

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // import functions and grab DOM elements
+const inputNoun1 = document.getElementById('noun1');
+const inputTime = document.getElementById('time');
+const inputVerb1 = document.getElementById('verb1');
+const inputProf = document.getElementById('professional-title1');
+const inputCity1 = document.getElementById('city-name1');
 
 // initialize state
 
